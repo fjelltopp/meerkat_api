7d03f3f0561c74ac2d7ac0bd5bd3780afa74f3b8
gulfa <gunnarroe@gmail.com>
2017-01-24 14:42:54 +0200

    minor change

--- meerkat_api/__init__.py
+++ meerkat_api/__init__.py
@@ -245,7 +245,8 @@ api.add_resource(Completeness,
                  "/completeness/<variable>/<location>/<number_per_week>",
                  "/completeness/<variable>/<location>/<number_per_week>/<start_week>",
                  "/completeness/<variable>/<location>/<number_per_week>/<start_week>/<exclude>",
-                 "/completeness/<variable>/<location>/<number_per_week>/<start_week>/<exclude>/<weekend>")
+                 "/completeness/<variable>/<location>/<number_per_week>/<start_week>/<exclude>/<weekend>",
+                 "/completeness/<variable>/<location>/<number_per_week>/<start_week>/<exclude>/<weekend>/<end_date>")
 api.add_resource(Records, "/records/<variable>/<location_id>")
 
 @app.route('/')
