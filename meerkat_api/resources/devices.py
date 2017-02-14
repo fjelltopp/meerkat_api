38644abd20cda5bf83b30e2d2e2b4661e4e55a61
jyri <Jyri Soppela>
2017-02-14 21:28:36 +0000

    Add interface to mobile_messaging

--- meerkat_api/resources/devices.py
+++ meerkat_api/resources/devices.py
@@ -3,10 +3,13 @@ Data resource for getting Device data
 """
 from flask_restful import Resource
 
-from meerkat_api import db
+from meerkat_api import db, app
 from meerkat_abacus import model
+import json
 
 from meerkat_api.authentication import authenticate
+
+from .. import common as c
 
 from meerkat_api.util import rows_to_dicts
 
@@ -20,8 +23,16 @@ class Devices(Resource):
     decorators = [authenticate]
 
     def get(self):
-        result = db.session.query(model.Devices)
-        if result:
-        	return rows_to_dicts(result)
+
+
+        
+        devices = db.session.query(model.Devices)
+        devices_dict=rows_to_dicts(devices)
+        if devices_dict:
+            device_ids=[]
+            for item in devices_dict:
+                device_ids.append(item["device_id"])
+            return_data = c.device_api(url='/device', data=json.dumps(device_ids))
+            return return_data
         else:
-        		return {}
+            return {"message":"No devices registered"}
