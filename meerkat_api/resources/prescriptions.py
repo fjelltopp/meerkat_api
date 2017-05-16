6b77af6c19469731097889b70884c202150b09bf
jyri <Jyri Soppela>
2017-05-16 15:56:46 +0100

    get prescriptions per clinic and medicine

--- meerkat_api/resources/prescriptions.py
+++ meerkat_api/resources/prescriptions.py
@@ -11,11 +11,11 @@ import pandas as pd
 from meerkat_api import db, app
 from meerkat_api.resources.epi_week import EpiWeek, epi_week_start, epi_year_start
 from meerkat_abacus.model import Data, Locations
-from meerkat_api.util import get_children, is_child
+from meerkat_api.util import get_children, is_child, fix_dates
 from meerkat_abacus.util import get_locations
 from meerkat_api.authentication import authenticate
-from meerkat_abacus.util import get_locations
 
+from meerkat_api.resources.explore import QueryVariable, get_variables
 
 class Prescriptions(Resource):
     """
@@ -24,9 +24,36 @@ class Prescriptions(Resource):
     Args: \n
         location: location id
             Returns:\n
-\n
     """
     decorators = [authenticate]
 
     def get(self, location, start_date = None, end_date = None):
-        return {"message":"Work in progress"}
+
+        start_date, end_date = fix_dates(start_date, end_date)
+
+        locs = get_locations(db.session)
+        clinics = get_children(parent = location, locations = locs, require_case_report = True)
+
+        barcode_category = 'barcode_prescription'
+
+        barcode_variables = get_variables(barcode_category)
+
+        date_conditions = [Data.date >= start_date, Data.date < end_date]
+
+        conditions = [Data.categories.has_key(barcode_category)] + date_conditions
+
+        
+        query = db.session.query(Data.clinic,
+                             Data.categories[barcode_category].astext,func.count(Data.id)).filter(
+                                 *conditions).group_by(Data.clinic, Data.categories[barcode_category].astext)
+
+        ret = query.all()
+
+        return ret
+        
+
+        #select clinic, categories->>'barcode_prescription', count(*) 
+        # from data where categories->>'barcode_prescription' is not null group by clinic, categories->>'barcode_prescription';
+
+
+        return str(ret)
