a041443a5ee5a7283eee3c611ea62bc2bdc97e46
Tomasz Sabała <sabala.tomek@gmail.com>
2017-07-28 18:11:35 +0200

    Working example of demo_case export w/o locations.

--- api_background/api_background/dhis2_export.py
+++ api_background/api_background/dhis2_export.py
@@ -1,5 +1,9 @@
 import requests
 import json
+from time import sleep
+
+from datetime import date
+
 from export_data import __get_keys_from_db
 from util import get_db_engine
 from meerkat_abacus.model import form_tables
@@ -13,6 +17,20 @@ if __name__ == "__main__":
     api_url = url + api_resource
     credentials = ('admin', 'district')
     headers = {"Content-Type": "application/json", "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="}
+
+    # TODO: for now only hardocded will be done in a more elegant way
+    program_id = 'ZU7Z7ouwbba'
+    # program_id_req = requests.get("{}programs?query={}".format(api_url, form), auth=credentials)
+    # program_id = list(program_id_req.json()['programs'][0].values())[0]
+    org_unit_id = 'wZxJHG0MUNz' # TODO: What is orgUnit in DHIS2 in WHO context?
+
+    # removing previously stored events
+    res = requests.get("{}events?program={}&orgUnit={}".format(api_url, program_id, org_unit_id), auth=credentials)
+    results = res.json()
+    for result in results["events"]:
+        event_id = result['event']
+        a_delete = requests.delete("{}events/{}".format(api_url, event_id), auth=credentials)
+        print("Deleted event {} with status {}".format(event_id, a_delete.status_code))
     for key in keys:
         continue
         res = requests.get("{}dataElements?query={}".format(api_url, key), auth=credentials)
@@ -30,21 +48,54 @@ if __name__ == "__main__":
             print(post_res.status_code)
             print(post_res.text)
 
-    event_payload = {}
-    data_elements = {}
-    results = session.query(form_tables[form].data).yield_per(1000)
+    data_values = []
+    results = session.query(form_tables[form].data).all()
 
-    result = results[0]
-    data = result.data
-    for key in keys:
-        key_dhis_id_req = requests.get("{}dataElements?query={}".format(api_resource, key, headers=headers))
-        dhis_key_id = key_dhis_id_req.json()
+    event_payload_list = []
+    for counter, result in enumerate(results):
+        event_payload = {}
+        data_values = []
+        data = result.data
+        s2 = requests.session()
+        print("Preparing event {}".format(str(counter)))
+        for key in keys:
+            if key in ["vaccination_type", "national"]:
+                continue
+            key_dhis_id_req = s2.get("{}dataElements?query={}".format(api_url, key), auth=credentials)
+            dhis_key_id = key_dhis_id_req.json()['dataElements'][0]['id']
+            _temp = {}
+            if key in data.keys():
+                # data_values.append({'dataElement' : dhis_key_id, 'value' : key + "_" + str(counter)})
+                data_values.append({'dataElement' : dhis_key_id, 'value' : data[key]})
+        event_payload['dataValues'] = data_values
+        str_today = date.today().strftime("%Y-%m-%d")
+        status = 'COMPLETED'
+        stored_by = 'admin'
+
+        event_payload['program'] = program_id
+        event_payload['orgUnit'] = org_unit_id
+        event_payload['eventDate'] = str_today
+        event_payload['completedDate'] = str_today
+        event_payload['dataValues'] = data_values
+        event_payload['status'] = status
+
+        event_payload_list.append(event_payload)
+
+    payload = {}
+    payload['events'] = event_payload_list
+    json_event_payload = json.dumps(payload)
+
+    s = requests.session()
+    event_res = s.post("{}events".format(api_url), headers=headers, data=json_event_payload)
+    print(event_res.status_code)
+    print(event_res.text)
 
 
 
 
 
 
-    res = requests.get("%s%sdataElements" % (url, api_resource), auth=credentials)
-    print(res.status_code)
-    print(res.text)
+
+    # res = requests.get("%s%sdataElements" % (url, api_resource), auth=credentials)
+    # print(res.status_code)
+    # print(res.text)
