f7c54d7254b5f757526489f0c6b2dd368d6f2c20
Jonathan Berry <jonathan.s.berry@googlemail.com>
2017-08-01 11:34:07 +0100

    Revise null island configs.

--- country_config/null_config.py
+++ country_config/null_config.py
@@ -1,5 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@dev_db_1/meerkat_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@db/meerkat_db'
 API_KEY = "test-api"
-AUTH = {
-    'default': [['registered'],['demo']]
-}
+AUTH = {'default': [['registered'], ['demo']]}
