dd548bbde8dfd45c9e4013b61949e646ed8574df
Tomasz Sabała <sabala.tomek@gmail.com>
2017-08-01 12:39:26 +0200

    Fixed incorrect dataElements export. Added batch event delete.

--- api_background/api_background/dhis2_export.py
+++ api_background/api_background/dhis2_export.py
@@ -9,49 +9,62 @@ from meerkat_abacus.model import form_tables
 
 
 def clear_old_events(program_id, org_unit_id):
-    res = requests.get("{}events?program={}&orgUnit={}".format(api_url, program_id, org_unit_id), auth=credentials)
+    events_id_list = []
+    res = requests.get("{}events?program={}&orgUnit={}&skipPaging=true".format(api_url, program_id, org_unit_id), auth=credentials)
+    # res = requests.get("{}events?program={}&orgUnit={}&pageSize={}&page={}".format(api_url, program_id, org_unit_id, 100, index), auth=credentials)
     results = res.json()
     for result in results["events"]:
         event_id = result['event']
-        a_delete = requests.delete("{}events/{}".format(api_url, event_id), auth=credentials)
-        print("Deleted event {} with status {}".format(event_id, a_delete.status_code))
+        events_id_list.append({'event' : event_id})
+    delete_json = json.dumps({"events" : events_id_list})
+    a_delete = requests.post("{}events?strategy=DELETE".format(api_url), data=delete_json, headers=headers)
+    print("Deleted old events with status {}".format(a_delete.status_code))
 
 
-def update_data_elements(keys):
-    for key in keys:
-        res = requests.get("{}dataElements?query={}".format(api_url, key), auth=credentials)
-        json_body = json.loads(res.text)
-        if int(json_body['pager']['total']) == 0:
-            payload = {}
-            payload['name'] = key
-            payload['shortName'] = key
-            payload['domainType'] = 'TRACKER'
-            payload['valueType'] = 'TEXT'
-            payload['aggregationType'] = 'NONE'
-            json_payload = json.dumps(payload)
+def update_data_elements(key, headers):
+    payload = {}
+    payload['name'] = key
+    payload['shortName'] = key
+    payload['domainType'] = 'TRACKER'
+    payload['valueType'] = 'TEXT'
+    payload['aggregationType'] = 'NONE'
+    json_payload = json.dumps(payload)
 
-            post_res = requests.post("{}dataElements".format(api_url), data=json_payload, headers=headers)
-            print(post_res.status_code)
-            print(post_res.text)
+    post_res = requests.post("{}dataElements".format(api_url), data=json_payload, headers=headers)
+    json_res = post_res.json()
+    # print(post_res.status_code)
+    # print(post_res.text)
+    uid = json_res['response']['uid']
+    print("Created data element \"{}\" with uid: {}".format(key, uid))
+    return uid
 
 
-def prepare_data_values(keys, dhis_keys):
+def prepare_data_values(keys, dhis_keys, data):
     data_values = []
-    data = result.data
     for key in keys:
-        if key in ["vaccination_type", "national"]:
-            # TODO: vaccination & vaccination_type give the same first result!!!
-            continue
+        # if key in ["vaccination_type", "national"]:
+        #     TODO: vaccination & vaccination_type give the same first result!!!
+            # continue
         if key in data.keys():
             data_values.append({'dataElement': dhis_keys[key], 'value': data[key]})
     return data_values
 
 
-def get_dhis2_keys(url, credentials, keys):
+def get_dhis2_keys(url, credentials, headers, keys):
     result = {}
+    dhis2_data_elements_res = requests.get("{}dataElements".format(url), auth=credentials)
+    dhis2_data_elements = dhis2_data_elements_res.json()['dataElements']
+    data_elements_ids = []
+    data_elements_names = []
+    for d in dhis2_data_elements:
+        data_elements_ids.append(d['id'])
+        data_elements_names.append(d['displayName'])
+    data_element_lookup = dict(zip(data_elements_names, data_elements_ids))
     for key in keys:
-        key_dhis_id_req = requests.get("{}dataElements?query={}".format(url, key), auth=credentials)
-        dhis_key_id = key_dhis_id_req.json()['dataElements'][0]['id']
+        if key in data_element_lookup:
+            dhis_key_id = data_element_lookup[key]
+        else:
+            update_data_elements([key], headers)
         result[key] = dhis_key_id
     return result
 
@@ -72,14 +85,15 @@ if __name__ == "__main__":
     api_resource = "/api/26/"
     api_url = url + api_resource
     credentials = ('admin', 'district')
+    headers = {"Content-Type": "application/json", "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="}
 
     db, session = get_db_engine()
     keys = __get_keys_from_db(db, form)
-    dhis_keys = get_dhis2_keys(api_url, credentials, keys)
-    headers = {"Content-Type": "application/json", "Authorization": "Basic YWRtaW46ZGlzdHJpY3Q="}
+    dhis_keys = get_dhis2_keys(api_url, credentials, headers, keys)
 
     # TODO: for now only hardocded will be done in a more elegant way
     program_id = 'ZU7Z7ouwbba'
+    # program_id = 'T6VaKGprnc5'
     # program_id_req = requests.get("{}programs?query={}".format(api_url, form), auth=credentials)
     # program_id = list(program_id_req.json()['programs'][0].values())[0]
     org_unit_id = 'wZxJHG0MUNz' # TODO: What is orgUnit in DHIS2 in WHO context?
@@ -88,7 +102,6 @@ if __name__ == "__main__":
     stored_by = 'admin'
 
     clear_old_events(program_id, org_unit_id)
-    update_data_elements(keys)
 
     data_values = []
     results = session.query(form_tables[form].data).all()
@@ -96,7 +109,7 @@ if __name__ == "__main__":
     event_payload_list = []
     for counter, result in enumerate(results):
         event_payload = {}
-        data_values = prepare_data_values(keys, dhis_keys)
+        data_values = prepare_data_values(keys, dhis_keys, result.data)
 
         event_payload['dataValues'] = data_values
         event_payload['program'] = program_id
@@ -107,7 +120,7 @@ if __name__ == "__main__":
         event_payload['status'] = status
 
         event_payload_list.append(event_payload)
-        if counter % 1 == 0:
+        if counter % 100 == 0:
             send_events_batch(event_payload_list)
             event_payload_list = []
     send_events_batch(event_payload_list)
