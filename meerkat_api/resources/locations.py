0a6b8798147a114920bb3c5caa9953070bb6a67e
Jonathan Berry <jonathan.s.berry@googlemail.com>
2017-09-12 11:04:26 +0100

    Styling documentation and debugging.

--- meerkat_api/resources/locations.py
+++ meerkat_api/resources/locations.py
@@ -9,6 +9,7 @@ from meerkat_api.util import row_to_dict, rows_to_dicts, is_child, get_children
 from meerkat_api import db
 from meerkat_abacus import model
 from meerkat_abacus.util import get_locations
+import json
 
 
 class Locations(Resource):
@@ -63,19 +64,35 @@ class LocationTree(Resource):
     """
     Location Tree
 
+    Accepts optional GET args:
+        inc_case_types - A JSON list of case types to filter the location Tree
+            by. A location is only included in the tree if there is an
+            intersection between this list and the locations case_type list.
+            Used when viewing e.g. PIP data - the user is only interested in
+            viewing clinics/locations that are actually submitting PIP data.
+        exc_case_types - A JSON list of case types to filter the location tree
+            by.  In a similar, but opposite, way to inc_case_types, a location
+            is only included in the tree if there is NO intersection between
+            this list and the location's case_type list.
+
     Args:
-       only_case_reports: Only include clinics that submitt case reports
+       only_case_reports: Only include clinics that submit case reports
+
     Returns:
        Returns a location Tree
     """
     decorators = [authenticate]
 
     def get(self, only_case_reports=True):
-        with_case_types = request.args.get('with_case_types', [])
-        without_case_types = request.args.get('without_case_types', [])
+        # Load filters supplied in GET args
+        inc_case_types = json.loads(request.args.get('inc_case_types', '[]'))
+        exc_case_types = json.loads(request.args.get('exc_case_types', '[]'))
+
+        # Get location data from db and any access restrictions set by auth
         locs = get_locations(db.session)
         loc = g.allowed_location
 
+        # Start drawing the tree
         ret = {loc: {"id": loc, "text": locs[loc].name, "nodes": []}}
         for l in sorted(locs.keys()):
             if l >= loc and is_child(loc, l, locs):
@@ -91,26 +108,28 @@ class LocationTree(Resource):
                         ret[locs[l].parent_location]["nodes"].append(ret[l])
 
                     # Determine if the location matches incl and excl criteria
-                    loc_case_types = locs[l].case_type
-                    inc = bool(set(with_case_types) & set(loc_case_types))
-                    exc = bool(set(without_case_types) & set(loc_case_types))
+                    loc_case_types = set()
+                    if locs[l].case_type:
+                        loc_case_types = set(locs[l].case_type)
+                    inc = bool(set(inc_case_types) & loc_case_types)
+                    exc = bool(set(exc_case_types) & loc_case_types)
 
                     # Add the location if it is not a clinic
                     if not locs[l].level == 'clinic':
                         add_loc()
                     # Otherwise add the location if no filters provided at all
-                    elif not with_case_types and not without_case_types:
+                    elif not inc_case_types and not exc_case_types:
                         add_loc()
-                    # Otherwise add loc if both filters are provided and...
+                    # Otherwise if both filters are provided, only add loc if
                     # ...inclusion criteria is met but not exclusion criteria
-                    elif (with_case_types and without_case_types and
-                          (inc and not exc)):
-                        add_loc()
+                    elif inc_case_types and exc_case_types:
+                        if inc and not exc:
+                            add_loc()
                     # Otherwise add loc if incl criteria specified and met
-                    elif with_case_types and inc:
+                    elif inc_case_types and inc:
                         add_loc()
                     # Otherwise add loc if excl criteria specified and not met
-                    elif without_case_types and not exc:
+                    elif exc_case_types and not exc:
                         add_loc()
 
         # Recursively clean any branches without clinics in them.
