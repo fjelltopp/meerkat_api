887f5b395cf7db6984736441bab9ab002b29fe88
Jonathan Berry <jonathan.s.berry@googlemail.com>
2018-02-21 16:25:34 +0000

    Get API working with RTD.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,6 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute.
-import pip
-import logging
-import os
 import sys
 from unittest.mock import MagicMock
 
@@ -65,18 +62,6 @@ def mock_modules():
 
 
 mock_modules()
-
-
-def install_package():
-    sys.path.insert(0, os.path.abspath('../../'))
-    with open(os.path.abspath('../../requirements.txt')) as f:
-        for line in f:
-            try:
-                pip.main(['install', line])
-            except Exception:
-                logging.warning("Failed to install {}".format(line))
-
-install_package()
 
 
 # -- General configuration ------------------------------------------------
