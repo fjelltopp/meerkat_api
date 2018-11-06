4b5b3ee38bbc725013b1c39dfb1ca4a0bfe5b089
gulfa <gunnarroe@gmail.com>
2018-11-06 09:21:55 +0100

    Adding missing celery config.

---
+++ api_background/api_background/celery_app.py
@@ -0,0 +1,49 @@
+"""
+Main Celery App file
+
+"""
+from raven.contrib.celery import register_signal, register_logger_signal
+import celery
+import logging
+import raven
+import os
+import time
+import shutil
+
+from api_background import celeryconfig
+
+# class Celery(celery.Celery):
+#     def on_configure(self):
+#         if config.sentry_dns:
+#             client = raven.Client(config.sentry_dns)
+#             # register a custom filter to filter out duplicate logs
+#             register_logger_signal(client)
+#             # hook into the Celery error handler
+#             register_signal(client)
+
+logging.getLogger().setLevel(logging.INFO)
+app = celery.Celery()
+app.conf.task_default_queue = 'api_background'
+app.config_from_object(celeryconfig)
+
+import api_background.export_data
+
+
+@celery.task
+def cleanup_downloads():
+    folder = '/var/www/meerkat_api/api_background/api_background/exported_data'
+    downloads = os.listdir(folder)
+    oldest = time.time() - 3600
+    for download in downloads:
+        path = '{}/{}'.format(folder, download)
+        if os.stat(path).st_mtime < oldest:
+            try:
+                shutil.rmtree(path)
+            except NotADirectoryError:
+                pass
+
+
+
+if __name__ == "__main__":
+    app.start()
+
---
+++ api_background/api_background/celeryconfig.py
@@ -0,0 +1,32 @@
+"""
+Celery configuration file
+
+To setup celery properly we need the broker and backend urls, which
+can be set by environment variable MEERKAT_BROKER_URL.
+
+In this config we set up a beat schedule with a timedelta in seconds
+given by the config.interval value.
+
+"""
+
+import os
+from celery.schedules import crontab
+
+BROKER_URL = 'amqp://guest@rabbit//'
+CELERY_RESULT_BACKEND = 'rpc://guest@rabbit//'
+
+new_url = os.environ.get("MEERKAT_BROKER_URL")
+if new_url:
+    BROKER_URL = new_url
+    CELERY_RESULT_BACKEND = new_url
+
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json', 'yaml']
+CELERY_ENABLE_UTC = True
+CELERYD_MAX_TASKS_PER_CHILD = 1  # To help with memory constraints
+CELERYBEAT_SCHEDULE = {}
+CELERYBEAT_SCHEDULE['cleanup_downloads'] = {
+    'task': 'meerkat_abacus.tasks.cleanup_downloads',
+    'schedule': crontab(minute=16, hour='*')
+}
