8c946b3c895296fb443fe4578e63a662d2e70067
Tomasz Sabała <sabala.tomek@gmail.com>
2020-10-05 14:04:25 +0200

    Fixup, api_background uses parent meerkat_api requirements.txt file.

--- api_background/setup.py
+++ api_background/setup.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 import uuid
-import os
 from setuptools import setup, find_packages
-import pathlib
-import pkg_resources
-pwd = pathlib.Path(__file__).parent.absolute()
-with pathlib.Path(os.path.join(pwd, '../requirements.txt')).open() as requirements_txt:
-    reqs = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setup(
     name='Meerkat API Background tasks',
@@ -19,6 +9,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=reqs,
+    # you need to manually install meerkat_api/requirements.txt
+    install_requires=[],
     test_suite='api_background.test'
 )
